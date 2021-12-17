91c0661fc6fc780d0cd3e0bbcef1f2a9d187e868
houseme <qzg40737@163.com>
2021-12-18 01:12:01 +0800

    feat:merge upstream/master

--- apiv1/captcha.go
+++ apiv1/captcha.go
@@ -5,7 +5,7 @@ import (
 )
 
 type CaptchaIndexReq struct {
-	g.Meta `path:"/captcha" method:"get" tags:"验证码" summary:"获取默认的验证码" dc:"注意直接返回的是图片二进制内容"`
+	g.Meta `path:"/captcha" method:"get" tags:"工具" summary:"获取默认的验证码" dc:"注意直接返回的是图片二进制内容"`
 }
 type CaptchaIndexRes struct {
 	g.Meta `mime:"png" dc:"验证码二进制内容" `
---
+++ internal/consts/openapi.go
@@ -0,0 +1,18 @@
+package consts
+
+const (
+	OpenAPITagNameIndex    = `首页`
+	OpenAPITagNameLogin    = `登录`
+	OpenAPITagNameRegister = `注册`
+	OpenAPITagNameArticle  = `文章`
+	OpenAPITagNameTopic    = `话题`
+	OpenAPITagNameAsk      = `问答`
+	OpenAPITagNameReply    = `回复`
+	OpenAPITagNameContent  = `内容`
+	OpenAPITagNameSearch   = `搜索`
+	OpenAPITagNameInteract = `交互`
+	OpenAPITagNameCategory = `分类`
+	OpenAPITagNameProfile  = `个人`
+	OpenAPITagNameUser     = `用户`
+	OpenAPITagNameMess     = `工具`
+)
